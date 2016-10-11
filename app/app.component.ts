dd39d01f6a843dec3ac2db1de97561c82f3a57cf
marcblakely <marcblakely@comcast.net>
2016-10-11 08:22:00 -0400

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'mb-signon',
+  templateUrl: 'app/shared/star.component.html',
+  styleUrls: ['app/shared/star.component.css']
+  
+})
+export class AppComponent { }
